d53b6c753080c3e832d8b1e11b31c3a97e18656b
Marcella <135839101+marcella2808@users.noreply.github.com>
2025-05-16 08:14:39 -0300

    Update test_batalha.py

--- test_batalha.py
+++ test_batalha.py
@@ -1,5 +1,5 @@
 from batalha import heroi_vs_vilao
 
 def test_heroi_ganha():
-    vitorias = [heroi_vs_vilao() for _ in range(5)]
+    vitorias = [heroi_vs_vilao() for _ in range(10)]
     assert "heroi" in vitorias, "Nenhum herói venceu! O build está corrompido!"
